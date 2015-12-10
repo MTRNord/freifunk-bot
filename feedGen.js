4bb74ca65b97150fd9395f33c2d10bdbffa3b6d3
Marcel Radzio <marcel@radzio-sh.de>
2015-12-10 19:53:12 +0100

    Update script

--- feedGen.js
+++ feedGen.js
@@ -1,5 +1,16 @@
 var request = require('request');
 var curl = require('curlrequest');
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+var door_status2 = "1";
+function MakePush(){
 request.get('http://www.nordlab-ev.de/doorstate/status.txt', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var door_status = body;
@@ -8,18 +19,26 @@ request.get('http://www.nordlab-ev.de/doorstate/status.txt', function (error, re
       var door_status = error;
       //console.log(error);
     }
-    console.log(door_status);
-    if (door_status == "geschlossen"){
-      door_status = "closed";
-    }else{
-      door_status = "open";
+    console.log("Current Real State:" + door_status + "\n");
+    console.log("Current Last State:" + door_status2 + "\n");
+    console.log("Next Check!");
+    if (door_status2 !== door_status){
+      door_status2 = door_status;
+      if (door_status == "geschlossen"){
+        door_status = "closed";
+      }else{
+        door_status = "open";
+      }
+
+      curl.request({ url: 'https://api.pushbullet.com/v2/pushes', method: 'POST', headers: { "Access-Token": 'v1TpGyUfIoydWW1KZa4KWfjmMcfnDYyTdGuju3ZH58Y7E', "Content-Type": 'application/json' }, data: '{"channel_tag": "space-door", "body":"'+ door_status + '","title":"Door Status","type":"note"}'}, function (err, stdout, meta) {
+        console.log(err);
+        console.log('\n STDOUT: \n');
+        console.log(stdout);
+      });
     }
-
-  curl.request({ url: 'https://api.pushbullet.com/v2/pushes', method: 'POST', headers: { "Access-Token": 'v1TpGyUfIoydWW1KZa4KWfjmMcfnDYyTdGuju3ZH58Y7E', "Content-Type": 'application/json' }, data: '{"channel_tag": "space-door", "body":"'+ door_status + '","title":"Door Status","type":"note"}'}, function (err, stdout, meta) {
-    console.log(err);
-    console.log('\n STDOUT: \n');
-    console.log(stdout);
-  });
-
-
+    console.log("WAIT!");
+    sleep(9*1000);
+    MakePush();
 });
+}
+MakePush();
