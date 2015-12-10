3d99e26551c7d8a55eb66ea9a9237a543c0d2775
Marcel Radzio <marcel@radzio-sh.de>
2015-12-10 22:37:51 +0100

    try to fix irc libary

--- handlers/irc.js
+++ handlers/irc.js
@@ -1,13 +1,16 @@
+var config = {
+	channels: ["#hackerspace"],
+	server: "irc.lugfl.de",
+	botName: "DoorBot"
+};
 var irc = require('irc');
 module.exports = {
 	ircSend: function (door_status){
-		var config = {
-			channels: ["#hackerspace"],
-			server: "irc.lugfl.de",
-			botName: "DoorBot"
-		};
 		var bot = new irc.Client(config.server, config.botName, {
 			channels: config.channels
+		});
+		bot.addListener('error', function(message) {
+    		console.log('error: ', message);
 		});
 		bot.say('#hackerspace', "Door Staus changed to: " + door_status);
 	}
