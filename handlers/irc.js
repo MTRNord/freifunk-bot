43d656389449023ca84a9ddeabb35743431d6f05
Marcel Radzio <marcel@radzio-sh.de>
2015-12-10 23:27:55 +0100

    maybe fixed the irc bot

--- handlers/irc.js
+++ handlers/irc.js
@@ -9,11 +9,16 @@ module.exports = {
 		bot.addListener('error', function(message) {
     		console.log('error: ', message);
 		});
-		bot.connect();
-		bot.send('msg', 'NickServ identify DoorBot');
-		bot.say('#hackerspace', 'Door Bot is starting to watch on the Door Status');
+		bot.connect(5, function() {
+    		bot.send('msg', 'NickServ identify DoorBot');
+			bot.say('#hackerspace', 'Door Bot is starting to watch on the Door Status');
+		});
+		
 	},
 	ircSend: function (door_status){
-		bot.send('say', 'Door Status changed to: ' + door_status);
+		bot.connect(5, function() {
+    		bot.send('say', 'Door Status changed to: ' + door_status);
+		});
+		
 	}
 };
