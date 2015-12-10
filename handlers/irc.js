c5dad266f81a70fbd2ff348ae16b8aa4fd513a4c
Marcel Radzio <marcel@radzio-sh.de>
2015-12-10 23:25:44 +0100

    maybe fixed the irc bot

--- handlers/irc.js
+++ handlers/irc.js
@@ -9,7 +9,9 @@ module.exports = {
 		bot.addListener('error', function(message) {
     		console.log('error: ', message);
 		});
-		bot.send('say', 'Door Bot is starting to watch on the Door Status');
+		client.connect();
+		bot.send('msg', 'NickServ identify DoorBot');
+		bot.say('#hackerspace', 'Door Bot is starting to watch on the Door Status');
 	},
 	ircSend: function (door_status){
 		bot.send('say', 'Door Status changed to: ' + door_status);
