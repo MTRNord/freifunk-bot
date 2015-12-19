503608c027a7cebe8cf82bd5efd9a033850235e8
Marcel Radzio <marcel@radzio-sh.de>
2015-12-19 01:00:01 +0100

    Some optimizings

--- handlers/pushbullet.js
+++ handlers/pushbullet.js
@@ -2,9 +2,9 @@ var curl = require('curlrequest');
 module.exports = {
 	pushbulletSend: function (door_status){
 		curl.request({ url: 'https://api.pushbullet.com/v2/pushes', method: 'POST', headers: { "Access-Token": 'Access-Token', "Content-Type": 'application/json' }, data: '{"channel_tag": "space-door", "body":"'+ door_status + '","title":"Door Status","type":"note"}'}, function (err, stdout, meta) {
-			console.log(err);
-			console.log('\n STDOUT: \n');
-			console.log(stdout);
+			if (err){
+				console.log(err);
+			}
 		});
 	}
 };
