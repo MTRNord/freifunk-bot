c0db73742978303e833282834b350af5ed52bf25
Marcel Radzio <marcel@radzio-sh.de>
2015-12-29 21:03:26 +0100

    Start working on v0.0.2 and adding Documentation

---
+++ apidocs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "GetData"
+    ],
+    "modules": [
+        "Main"
+    ],
+    "allModules": [
+        {
+            "displayName": "Main",
+            "name": "Main",
+            "description": "Main-functionality for handling the Modules :)"
+        }
+    ],
+    "elements": []
+} };
+});
